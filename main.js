2e5d47ef6e9274f5ac341ec473d64f43e85b55db
mostafamohamedmostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2020-02-05 13:46:51 +0200

    adding js steps

---
+++ main.js
@@ -0,0 +1,57 @@
+var cardsArray = [
+    {    'name': 'CSS',    'img': 'https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true',  },
+    {    'name': 'HTML',    'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true',  },
+    {    'name': 'jQuery',    'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true',  },
+    {    'name': 'JS',    'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true',  },
+    {    'name': 'Node',    'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true',  },
+    {    'name': 'Photo Shop',    'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true',  },
+    {    'name': 'PHP',    'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true',  },
+    {    'name': 'Python',    'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true',  },
+    {    'name': 'Ruby',    'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true',  },
+    {    'name': 'Sass',    'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true',  },
+    {    'name': 'Sublime',    'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true',  },
+    {    'name': 'Wordpress',    'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true',  },
+  ];
+
+  //grab the div with id game-board and assign to a variable game
+
+  //create new section element and assign it to variable grid
+
+
+  //give section grid a class name of grid also
+
+
+  //append the grid section to te game board div
+
+
+
+  //
+
+
+
+  //
+
+
+
+
+
+
+  //
+
+
+
+
+
+
+  //
+
+
+
+
+
+
+
+
+
+
+  //
